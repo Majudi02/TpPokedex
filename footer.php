d2ee28542ef9ea115d96f6de8a150bfa0ae2b960
Lara <lara.rs1412@gmail.com>
2025-04-24 15:52:24 -0300

    Agregado header y footer de la pagina principal(sin loguearse)

---
+++ footer.php
@@ -0,0 +1,8 @@
+    <footer  class=" text-center text-muted py-3 mt-auto" style="background-color: #d2f4ea;">
+
+        <p class="mb-0">&copy; 2025 Pokedex- Todos los derechos reservados</p>
+
+    </footer>
+</body>
+</html>
+
