31e24b70d3710ae66fe500c975866f21f64666b4
Mateo <mateodinu02@gmail.com>
2025-05-03 13:30:33 -0300

    Modificacion del encontrar Pokemon

--- encontrar_pokemon.php
+++ encontrar_pokemon.php
@@ -4,42 +4,18 @@ function encontrarPokemon($database){
     if (!isset($_POST["pokemonBuscado"])) return null;
     $textoABuscar = strtolower($_POST["pokemonBuscado"]);
 
-    if (is_numeric($textoABuscar)) {
-        return buscarPokemonPorNumero($database, $textoABuscar);
-    }
     if ($textoABuscar == "agua" || $textoABuscar == "electrico" || $textoABuscar == "planta" || $textoABuscar == "fuego") {
         return buscarPokemonPorTipo($database, $textoABuscar);
     }
 
-    return buscarPokemonPorNombre($database, $textoABuscar);
+    $resultadoPokemonDb = $database->query("SELECT * FROM pokemones WHERE nombre='$textoABuscar'OR id = '$textoABuscar'");
+
+    if(empty($resultadoPokemonDb)) return false;
+
+    return $resultadoPokemonDb;
 }
 
-function buscarPokemonPorNumero($database, $numeroABuscar)
-{
-    $resultadoPokemonDb = $database->query("SELECT * FROM pokemones WHERE id = '$numeroABuscar'");
-    if (empty($resultadoPokemonDb)) {
-        return false;
-    } else {
-        return $resultadoPokemonDb;
-    }
-}
 function buscarPokemonPorTipo($database, $tipo){
-    $resultadoTipoDb = $database->query("SELECT * FROM tipo WHERE tipo ='$tipo'");
-    if (empty($resultadoTipoDb)) return false;
-
-    $tipoId = $resultadoTipoDb[0]["id"];
-
-    $pokemonesBuscados = $database->query("SELECT * FROM pokemones WHERE tipo_id ='$tipoId'");
-
+    $pokemonesBuscados = $database->query("SELECT * FROM pokemones JOIN tipo ON pokemones.tipo_id= tipo.id  WHERE tipo ='$tipo'");
     return $pokemonesBuscados;
-}
-function buscarPokemonPorNombre($database, $nombrePokemon)
-{
-    $resultadoPokemonDb = $database->query("SELECT * FROM pokemones WHERE nombre = '$nombrePokemon'");
-
-    if (empty($resultadoPokemonDb)) {
-        return false;
-    } else {
-        return $resultadoPokemonDb;
-    }
 }
